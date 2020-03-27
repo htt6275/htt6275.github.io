3dddacfd801d0fe8b4ddd5cb29f6be8c8c17c232
tongtong <tongtong.he@sporit.cn>
2020-03-27 17:24:55 +0800

    xxx

--- common/index.js
+++ common/index.js
@@ -2,12 +2,10 @@ window.onload = () => {
     const data = {};
     const $ = window.document.querySelector.bind(window.document);
 };
-document.addEventListener('DOMContentLoaded', function() {
-    function handleCalc() {
-        var dw = document.body.clientWidth;
-        var minScale = dw / 75;
-        document.documentElement.style.fontSize = minScale + 'px';
-    }
-    handleCalc();
-    window.addEventListener('resize', handleCalc);
-});
+function handleCalc() {
+    var dw = document.body.clientWidth;
+    var minScale = dw / 75;
+    document.documentElement.style.fontSize = minScale + 'px';
+}
+handleCalc();
+window.addEventListener('resize', handleCalc);
