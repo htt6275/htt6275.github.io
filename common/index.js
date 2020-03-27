745d3960ac9ee655ab442658a187c0a0dfa0d611
tongtong <tongtong.he@sporit.cn>
2020-03-27 22:27:56 +0800

    修改页面

--- common/index.js
+++ common/index.js
@@ -1,16 +1,3 @@
-// window.onload = () => {
-//     const data = {};
-//     const $ = window.document.querySelector.bind(window.document);
-// };
-// document.addEventListener('DOMContentLoaded', function() {
-//     function handleCalc() {
-//         var dw = document.body.clientWidth;
-//         var minScale = dw / 75;
-//         document.documentElement.style.fontSize = minScale + 'px';
-//     }
-//     handleCalc();
-//     window.addEventListener('resize', handleCalc);
-// });
 window.onload = () => {
     const data = {};
     const $ = window.document.querySelector.bind(window.document);
